f61182bc0505f40b903b62baaa65e1b4f658d118
anam <anam.hossain@multidots.com>
2023-02-06 10:27:45 +0600

    fix call to action image issue and add debugger option in gulpfile

--- src/blocks/call-to-action/edit.js
+++ src/blocks/call-to-action/edit.js
@@ -1,234 +1,235 @@
-import {__} from "@wordpress/i18n";
+import { __ } from '@wordpress/i18n';
 import React from 'react';
-import { 
-    Button, 
-    ResponsiveWrapper, 
-    PanelBody,  
-    ButtonGroup
+import {
+	Button,
+	ResponsiveWrapper,
+	PanelBody,
+	ButtonGroup,
 } from '@wordpress/components';
 import { closeSmall, image as icon } from '@wordpress/icons';
-import SidebarControl from "./sidebarControl";
-import {isBlobURL} from "@wordpress/blob";
+import SidebarControl from './sidebarControl';
+import { isBlobURL } from '@wordpress/blob';
 import {
-    MediaUpload,
-    MediaUploadCheck,
-    useBlockProps,
-    InspectorControls,
-    ColorPalette,
-    AlignmentToolbar,
-    BlockIcon,
-    RichText,
-    MediaPlaceholder,
-    InnerBlocks
-} from "@wordpress/block-editor";
-import classnames from "classnames";
+	MediaUpload,
+	MediaUploadCheck,
+	useBlockProps,
+	InspectorControls,
+	ColorPalette,
+	AlignmentToolbar,
+	BlockIcon,
+	RichText,
+	MediaPlaceholder,
+	InnerBlocks,
+} from '@wordpress/block-editor';
+import classnames from 'classnames';
 
 export default function edit(props) {
-    // console.log('props ', props);
-    const {attributes, setAttributes, className, isSelected} = props;
-    
-    const onSelectImage = (imageObj) => {
-        // console.log('imageObj', imageObj);
-        setAttributes({
-            media: imageObj,
-            mediaId: imageObj.id,
-            mediaUrl: imageObj.url
-        })
-    }
-    const removeImage = () => {
-        setAttributes({
-            media: null,
-            mediaId: 0,
-            mediaUrl: ""
-        })
-    }
-    const onChangeTitle = (newTitle) => {
-        setAttributes({ 
-            title: newTitle
-        })
-    }
-    const onChangeContent = (newContent) => {
-        setAttributes({
-            content: newContent
-        })
-    }
-    const onChangeAlignment = (newAlignment) => {
-        setAttributes({
-            alignment: 'undefined' === newAlignment  ? none : newAlignment,
-        })
-    }
-    const renderPlaceholderForImage = () => {
-        return(
-            <div className="call-to-action-image-placeholder-wrapper">
-                <MediaPlaceholder
-                    allowedTypes={ [ 'image' ] }
-                    multiple={ false }
-                    icon="format-image"
-                    labels={ {
-                        title: ' ',
-                    } }
-                    onSelect={ ( imageEntity ) => {
-                        // debugger;
-                        console.log(imageEntity);
-                        if ( isBlobURL( imageEntity?.url ) ) {
-                            return;
-                        }
-                        setAttributes( {
-                            url: imageEntity?.url,
-                            alt: imageEntity?.alt,
-                            media: imageEntity,
-                            mediaId: imageEntity?.id,
-                            mediaUrl: imageEntity?.url
-                        } );
-                    }
-                    }
-                />
-            </div>
-        )
-    }
-    const renderImage = () => {
-        const {
-            focalPoint
-        } = attributes;
-        // console.log("focalPoint", focalPoint)
-        const classes = classnames('call-to-action-image-wrapper',{
-            'is-selected': isSelected
-        });
-        return(
-            <>
-                <div className={ classes }>
-                    {
-                        isSelected && (
-                            <ButtonGroup 
-                            className="block-library-gallery-item__inline-menu is-right is-visible"
-                            >
-                                <Button
-                                    icon={closeSmall}
-                                    onClick={ () => {
-                                        setAttributes({
-                                            media: '',
-                                            mediaUrl: ''
-                                        })
-                                    } }
-                                    label={__('Remove Image', 'anam-gutenberg-starter')}
-                                    disabled={ !isSelected }
-                                />
-                            </ButtonGroup>
-                        )
-                    }
-                    {/* <div 
-                        className="call-to-action-media-wrapper"
-                        style={{ textAlign: props.attributes.alignment }}
-                    > */}
-                        {
-                            props.attributes.media 
-                            &&
-                            <img 
-                            src={props.attributes.mediaUrl} 
-                            alt=""
-                            style={{
-                                objectPosition: focalPoint
-                                ? `${focalPoint.x * 100}% ${focalPoint.y * 100}%`
-                                : undefined
-                            }} 
-                            />
-                        }
-                    {/* </div> */}
-                </div>
-            </>
-        )
+	// debugger;
+	console.log('edit props ', props);
+	const { attributes, setAttributes, className, isSelected } = props;
 
-    }
-    return (
-        <div {...useBlockProps()}>
-            <SidebarControl
-                data={props}
-                onSelectImage={onSelectImage}
-                removeImage={removeImage}
-                onChangeAlignment={onChangeAlignment}
-            />
-            
-            <div>
-                
-                {/* {
-                    !props.attributes.media
-                    &&
-                    <MediaUploadCheck>
-                        <MediaUpload
-                            onSelect={ onSelectImage }
-                            value={props.attributes.mediaUrl}
-                            render={ ( { open } ) => (
-                                <Button onClick={ open }>
-                                    {
-                                        props.attributes.mediaUrl 
-                                        ? 
-                                        <img src={props.attributes.mediaUrl} alt="" />
-                                        : 
-                                        <BlockIcon icon={ icon } />
-                                    }
-                                </Button>
-                            ) }
-                            />
-                    </MediaUploadCheck>
-                } */}
-                
-                <div className="call-to-action">
-                    <div 
-                        className="call-to-action-media-wrapper"
-                        style={{ textAlign: props.attributes.alignment }}
-                    >
-                        {/* {
-                            props.attributes.media 
-                            &&
-                            <img src={props.attributes.mediaUrl} alt="" />
-                        } */}
-                        {
-                            props.attributes.showImage 
-                            ?
-                                props.attributes.media 
-                                ? renderImage()
-                                : renderPlaceholderForImage()
-                            : 
-                                false
-                        }
-                    </div>
-                    <div className="call-to-action-title">
-                        <RichText
-                            style={{ textAlign: props.attributes.alignment }}
-                            tagName="h3"
-                            onChange={onChangeTitle} 
-                            value={props.attributes.title && props.attributes.title}
-                        />
-                    </div>
-                    <div className="call-to-action-content">
-                        <RichText
-                            style={{ textAlign: props.attributes.alignment }}
-                            tagName="p"
-                            onChange={onChangeContent}
-                            value={props.attributes.content && props.attributes.content}
-                        />
-                    </div>
-                    <div 
-                    className="call-to-action-button"
-                    style={{ textAlign: props.attributes.alignment }}
-                    >
-                        <InnerBlocks
-                            template={[
-                                [
-                                    'core/button', 
-                                    {
-                                        placeholder:__('Action Link', 'anam-gutenberg-starter')
-                                    }
-                                ],
-                            ]}   
-                            allowedBlocks={['core/button']}
-                            templateInsertUpdatesSelection={true}
-                            __experimentalCaptureToolbars={true}
-                        />
-                        
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	const onChangeTitle = (newTitle) => {
+		setAttributes({
+			title: newTitle,
+		});
+	};
+	const onChangeContent = (newContent) => {
+		setAttributes({
+			content: newContent,
+		});
+	};
+	const onChangeAlignment = (newAlignment) => {
+		setAttributes({
+			alignment: 'undefined' === newAlignment ? none : newAlignment,
+		});
+	};
+	const onSelectImage = (imageObj) => {
+		debugger;
+		console.log('imageObj', imageObj);
+		setAttributes({
+			media: imageObj,
+			mediaId: imageObj.id,
+			mediaUrl: imageObj.url,
+		});
+	};
+	const removeImage = () => {
+		setAttributes({
+			media: null,
+			mediaId: 0,
+			mediaUrl: '',
+		});
+	};
+	const renderPlaceholderForImage = () => {
+		return (
+			<div className="call-to-action-image-placeholder-wrapper">
+				<MediaPlaceholder
+					allowedTypes={['image']}
+					multiple={false}
+					icon="format-image"
+					labels={{
+						title: 'The Image',
+					}}
+					onSelect={(imageEntity) => {
+						// debugger;
+						console.log(imageEntity);
+						if (isBlobURL(imageEntity?.url)) {
+							return;
+						}
+						setAttributes({
+							url: imageEntity?.url,
+							alt: imageEntity?.alt,
+							media: imageEntity,
+							mediaId: imageEntity?.id,
+							mediaUrl: imageEntity?.url,
+						});
+						console.log('updated attributes ', props.attributes);
+						// debugger;
+					}}
+				/>
+			</div>
+		);
+	};
+	const renderImage = () => {
+		console.log('render image', attributes);
+		const { focalPoint } = attributes;
+		console.log('focalPoint', focalPoint);
+		const classes = classnames('call-to-action-image-wrapper', {
+			'is-selected': isSelected,
+		});
+		return (
+			<>
+				<div className={classes}>
+					<div className="button-group">
+						{isSelected && (
+							<ButtonGroup className="block-library-gallery-item__inline-menu is-right is-visible">
+								<Button
+									icon={closeSmall}
+									onClick={() => {
+										setAttributes({
+											media: '',
+											mediaUrl: '',
+										});
+									}}
+									label={__(
+										'Remove Imagee',
+										'anam-gutenberg-starter'
+									)}
+									disabled={!isSelected}
+								/>
+							</ButtonGroup>
+						)}
+					</div>
+					<div
+						className="from-edit call-to-action-media-wrapper"
+						style={{ textAlign: props.attributes.alignment }}
+					>
+						{props.attributes.media && (
+							<img
+								src={props.attributes.mediaUrl}
+								alt=""
+								style={{
+									objectPosition: focalPoint
+										? `${focalPoint.x * 100}% ${
+												focalPoint.y * 100
+										  }%`
+										: undefined,
+								}}
+							/>
+						)}
+					</div>
+				</div>
+			</>
+		);
+	};
+	return (
+		<div {...useBlockProps()}>
+			<SidebarControl
+				data={props}
+				onSelectImage={onSelectImage}
+				removeImage={removeImage}
+				onChangeAlignment={onChangeAlignment}
+			/>
+
+			<div>
+				{/* <MediaUploadCheck>
+					{!props.attributes.media && (
+						<MediaUpload
+							onSelect={onSelectImage}
+							value={props.attributes.mediaUrl}
+							render={({ open }) => (
+								<Button onClick={open}>
+									{props.attributes.mediaUrl ? (
+										<img
+											src={props.attributes.mediaUrl}
+											alt=""
+										/>
+									) : (
+										<BlockIcon icon={icon} />
+									)}
+								</Button>
+							)}
+						/>
+					)}
+				</MediaUploadCheck> */}
+
+				<div className="call-to-action">
+					<div
+						className="call-to-action-media-wrapper"
+						style={{ textAlign: props.attributes.alignment }}
+					>
+						{props.attributes.media && (
+							<img src={props.attributes.mediaUrl} alt="" />
+						)}
+						{props.attributes.showImage
+							? props.attributes.media
+								? renderImage()
+								: renderPlaceholderForImage()
+							: false}
+					</div>
+					<div className="call-to-action-title">
+						<RichText
+							style={{ textAlign: props.attributes.alignment }}
+							tagName="h3"
+							onChange={onChangeTitle}
+							value={
+								props.attributes.title && props.attributes.title
+							}
+						/>
+					</div>
+					<div className="call-to-action-content">
+						<RichText
+							style={{ textAlign: props.attributes.alignment }}
+							tagName="p"
+							onChange={onChangeContent}
+							value={
+								props.attributes.content &&
+								props.attributes.content
+							}
+						/>
+					</div>
+					<div
+						className="call-to-action-button"
+						style={{ textAlign: props.attributes.alignment }}
+					>
+						<InnerBlocks
+							template={[
+								[
+									'core/button',
+									{
+										placeholder: __(
+											'Action Link',
+											'anam-gutenberg-starter'
+										),
+									},
+								],
+							]}
+							allowedBlocks={['core/button']}
+							templateInsertUpdatesSelection={true}
+							__experimentalCaptureToolbars={true}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/blocks/call-to-action/save.js
+++ src/blocks/call-to-action/save.js
@@ -1,54 +1,55 @@
 import React from 'react';
-import {
-    useBlockProps,
-    RichText,
-    InnerBlocks
-} from "@wordpress/block-editor";
+import { useBlockProps, RichText, InnerBlocks } from '@wordpress/block-editor';
 
 export default function save(props) {
-    const blockProps = useBlockProps.save({
-        className: "call-to-action"
-    })
-    return (
-        <div {...blockProps} >
-            { 
-                props.attributes.showImage && 
-                <div className="call-to-action-media-wrapper" style={{ textAlign: props.attributes.alignment }}>
-                    {
-                        props.attributes.media 
-                        &&
-                        <img 
-                        src={props.attributes.mediaUrl} 
-                        alt=""
-                        style={{
-                            objectPosition: props.attributes.focalPoint
-                            ? `${props.attributes.focalPoint.x * 100}% ${props.attributes.focalPoint.y * 100}%`
-                            : undefined
-                        }} 
-                        />
-                    }
-                </div>
-            }
-            <div className="call-to-action-title">
-                <RichText.Content
-                    style={{ textAlign: props.attributes.alignment }}
-                    tagName="h3"
-                    value={props.attributes.title && props.attributes.title }
-                />
-            </div>
-            <div className="call-to-action-content">
-                <RichText.Content
-                    style={{ textAlign: props.attributes.alignment }}
-                    tagName="p"
-                    value={props.attributes.content && props.attributes.content}
-                />
-            </div>
-            <div 
-            className="call-to-action-button" 
-            style={{ textAlign: props.attributes.alignment }}
-            >
-                <InnerBlocks.Content/>
-            </div>
-        </div>
-    )
+	console.log('save props', props);
+	const blockProps = useBlockProps.save({
+		className: 'call-to-action',
+	});
+	return (
+		<div {...blockProps}>
+			{props.attributes.showImage && (
+				<div
+					className="from-save call-to-action-media-wrapper"
+					style={{ textAlign: props.attributes.alignment }}
+				>
+					{props.attributes.media && (
+						<img
+							src={props.attributes.mediaUrl}
+							alt=""
+							style={{
+								objectPosition: props.attributes.focalPoint
+									? `${
+											props.attributes.focalPoint.x * 100
+									  }% ${
+											props.attributes.focalPoint.y * 100
+									  }%`
+									: undefined,
+							}}
+						/>
+					)}
+				</div>
+			)}
+			<div className="call-to-action-title">
+				<RichText.Content
+					style={{ textAlign: props.attributes.alignment }}
+					tagName="h3"
+					value={props.attributes.title && props.attributes.title}
+				/>
+			</div>
+			<div className="call-to-action-content">
+				<RichText.Content
+					style={{ textAlign: props.attributes.alignment }}
+					tagName="p"
+					value={props.attributes.content && props.attributes.content}
+				/>
+			</div>
+			<div
+				className="call-to-action-button"
+				style={{ textAlign: props.attributes.alignment }}
+			>
+				<InnerBlocks.Content />
+			</div>
+		</div>
+	);
 }
