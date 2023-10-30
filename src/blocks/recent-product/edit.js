a55b327ab5bb2425eb018db878fbc530aac2cd81
anam <anam.hossain@multidots.com>
2023-10-30 12:16:16 +0600

    [new] woocommerce recent product block added for javascript level

--- src/blocks/recent-product/edit.js
+++ src/blocks/recent-product/edit.js
@@ -1,5 +1,7 @@
 import { __ } from '@wordpress/i18n';
-import React from 'react';
+import React, { useEffect } from 'react';
+const WooCommerceRestApi = require('@woocommerce/woocommerce-rest-api').default;
+
 import {
 	TextControl,
 	ColorPicker,
@@ -9,6 +11,7 @@ import {
 	FontSizePicker,
 	CheckboxControl,
 } from '@wordpress/components';
+import apiFetch from '@wordpress/api-fetch';
 import { useState } from '@wordpress/element';
 import { more } from '@wordpress/icons';
 
@@ -24,6 +27,13 @@ import {
 } from '@wordpress/block-editor';
 
 const MY_TEMPLATE = [['core/button', { placeholder: 'Book Your Demo' }]];
+
+const api = new WooCommerceRestApi({
+	url: 'https://anamstarter.local/',
+	consumerKey: 'ck_c891525480173c17a6be34e0ff34797271bee966',
+	consumerSecret: 'cs_2960e30b1041ff1ddebae9746b95101c7e564f8a',
+	version: 'wc/v3',
+});
 
 const fontSizes = [
 	{
@@ -42,8 +52,44 @@ const fallbackFontSize = 16;
 export default function edit({ attributes, setAttributes }) {
 	const [fontSize, setFontSize] = useState(12);
 	const [isChecked, setChecked] = useState(true);
-	console.log('attributes', attributes);
-	console.log('fontSize', fontSize);
+	const [product, setProduct] = useState();
+	let productResponseData = '';
+	// console.log('process.env.WOO_CONSUMER_KEY', api);
+	useEffect(() => {
+		apiFetch({ path: '/wp/v2/posts' }).then((posts) => {
+			console.log(posts);
+		});
+		api.get('products', {
+			per_page: 3, // 20 products per page
+		})
+			.then((response) => {
+				setProduct(response.data);
+				setAttributes({
+					product_obj: response.data,
+				});
+				// Successful request
+				console.log('Response Status:', response.status);
+				console.log('Response Headers:', response.headers);
+				console.log('Response Data:', response.data);
+				console.log(
+					'Total of pages:',
+					response.headers['x-wp-totalpages']
+				);
+				console.log('Total of items:', response.headers['x-wp-total']);
+			})
+			.catch((error) => {
+				// Invalid request, for 4xx and 5xx statuses
+				console.log('Response Status:', error.response.status);
+				console.log('Response Headers:', error.response.headers);
+				console.log('Response Data:', error.response.data);
+			})
+			.finally(() => {
+				// Always executed.
+			});
+	}, []);
+
+	// console.log('.product_obj', .product_obj);
+	// console.log('fontSize', fontSize);
 	/**
 	 * pass style through useBlockProps()
 	 */
@@ -82,6 +128,8 @@ export default function edit({ attributes, setAttributes }) {
 			alignment: 'undefined' === newAlignment ? none : newAlignment,
 		});
 	};
+	console.log('productResponseData', product);
+	console.log('product obj', attributes && attributes.product_obj);
 	/**
 	 * return edit content
 	 */
@@ -94,7 +142,26 @@ export default function edit({ attributes, setAttributes }) {
 
 			{/* View */}
 			<div className="container gts_block__recent_product__container">
-				<h2>Recent Product</h2>
+				<h2>Recent Products</h2>
+				<div className="row">
+					{product &&
+						product.map(function (p, index) {
+							let onSale = p.on_sale;
+							return (
+								<div key={index} className="col-md-3">
+									<h2>
+										<a href={p.permalink}>{p.name}</a>
+									</h2>
+									<p
+										className="product_price"
+										dangerouslySetInnerHTML={{
+											__html: p.price_html,
+										}}
+									></p>
+								</div>
+							);
+						})}
+				</div>
 			</div>
 		</div>
 	);
--- src/blocks/recent-product/save.js
+++ src/blocks/recent-product/save.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import { __ } from '@wordpress/i18n';
+import React, { useEffect } from 'react';
 import { TextControl } from '@wordpress/components';
+
 import {
 	useBlockProps,
 	RichText,
@@ -11,6 +13,8 @@ import {
 } from '@wordpress/block-editor';
 
 export default function save(props) {
+	// console.log('hello world');
+	console.log('props', props);
 	/**
 	 * get block props and
 	 * assign it to a variable
@@ -24,7 +28,26 @@ export default function save(props) {
 	return (
 		<div {...blockProps}>
 			<div className="container gts_block__recent_product__container">
-				<h2>Recent Product</h2>
+				<h2>Recent Products</h2>
+				{/* <div className="row">
+					{props.attributes.product_obj &&
+						props.attributes.product_obj.map(function (p, index) {
+							let onSale = p.on_sale;
+							return (
+								<div key={index} className="col-md-3">
+									<h2>
+										<a href={p.permalink}>{p.name}</a>
+									</h2>
+									<p
+										className="product_price"
+										dangerouslySetInnerHTML={{
+											__html: p.price_html,
+										}}
+									></p>
+								</div>
+							);
+						})}
+				</div> */}
 			</div>
 		</div>
 	);
