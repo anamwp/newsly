ba50222aa85f506805dc825d4aa607691fac743b
anam <anam.hossain@multidots.com>
2024-02-03 19:27:07 +0600

    [new] breweries post type and shortcode added

--- includes/Init.php
+++ includes/Init.php
@@ -7,10 +7,12 @@ class Init {
 		 * init post types
 		 */
 		PostType\Demo::init();
+		PostType\Breweries::init();
 		/**
 		 * init shortcodes
 		 */
 		Shortcode\Demo::init();
+		Shortcode\Breweries::init();
 		/**
 		 * gutenberg block
 		 */
