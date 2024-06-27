b823aace311e12f4e0d20553547471d661a120cc
anam <anam.hossain@multidots.com>
2024-06-27 16:45:19 +0600

    [update] decouple ajax functionality of movie list into seperate file

--- includes/Blocks/Block.php
+++ includes/Blocks/Block.php
@@ -19,69 +19,17 @@ class Block {
 		}
 		return self::$instance;
 	}
+	public $movie_list_callback_instance;
 	/**
 	 * Undocumented function
 	 */
 	public function __construct() {
 		add_action( 'init', array( $this, 'register_starter_blocks' ) );
-		
-		add_action('wp_head', array( $this, 'handle_movie_pagination' ));
-		add_action( 'wp_ajax_nopriv_movie_pagination', array( $this, 'handle_movie_ajax_pagination' ) );
-		add_action( 'wp_ajax_movie_pagination', array( $this, 'handle_movie_ajax_pagination' ) );
+		$this->movie_list_callback_instance = Inc\Class_Movie_List_Callback::init();
 	}
-	public function handle_movie_pagination(){
-		?>
-		<script>
-			(function($){
-				$(document).ready(function(){
-					function getPageNumberFromUrl(url) {
-						// Create an anchor element to use the browser's built-in URL parsing
-						// var anchor = document.createElement('a');
-						// anchor.href = url;
-
-						// Extract query parameters from the URL
-						// var queryParams = getPageNumberFromUrl(anchor.href)
-						var queryParams = url.split('&');
-						// Iterate through query parameters to find 'page' parameter
-						for (var i = 0; i < queryParams.length; i++) {
-						var param = queryParams[i].split('=');
-						if (param[0] === 'page') {
-							return param[1];
-						}
-						}
-						// Return null if 'page' parameter is not found
-						return null;
-					}
-					$(document).on('click', '.cgl-ajax-number-pagination a', function(e){
-						e.preventDefault();
-						var page = $(this).attr('href');
-						var pageNumber = getPageNumberFromUrl(page);
-						console.log(pageNumber);
-						$.ajax({
-							url: anamajaxpagination.ajaxurl,
-							type: 'get',
-							data: {
-								action: 'movie_pagination',
-								pageNumber: pageNumber,
-							},
-							success: function(response){
-								console.log('response', response);
-							}
-						});
-					});
-				});
-			})(jQuery);
-		</script>
-		<?php
-	}
+	
 
 	public function register_starter_blocks() {
-		wp_enqueue_script('jquery');
-		// wp_enqueue_script('jquery', false, array(), false, false);
-
-		wp_localize_script( 'jquery', 'anamajaxpagination', array(
-			'ajaxurl' => admin_url( 'admin-ajax.php' ),
-		));
 		// $asset_file = require(ANAM_GUTENBERG_STARTER_DIR_URL . 'build/index.asset.php');
 		$asset_file = include ANAM_GUTENBERG_STARTER_PATH . '/build/index.asset.php';
 		/**
@@ -348,109 +296,11 @@ class Block {
 	}
 	public function movie_lists_render_frontend_callback($block_attributes, $content){
 		ob_start();
-		$get_movie_data = wp_remote_get( 'https://api.themoviedb.org/3/movie/popular?api_key=94413492db5e2e4ca5e93402ca623fca&language=en-US&page=1' );
-		$body = '';
-		$total_pages = '';
-		if ( is_array( $get_movie_data ) && ! is_wp_error( $get_movie_data ) ) {
-			$headers = $get_movie_data['headers']; // array of http header lines
-			$body    = json_decode($get_movie_data['body']); // use the content
-			$total_pages = $body->total_pages;
-		}
-		if($body):
-		?>
-			<div class="movie-list">
-				<?php 
-					foreach($body->results as $movie):
-				?>
-					<div class="movie-card">
-						<div class="movie-image">
-							<img src="https://image.tmdb.org/t/p/w500/<?php echo $movie->poster_path; ?>" alt="<?php echo $movie->title; ?>">
-						</div>
-						<div class="movie-content">
-							<h2><?php echo $movie->title; ?></h2>
-							<p class="overview"><?php echo $movie->overview; ?></p>
-						</div>
-					</div>
-				<?php
-					endforeach;
-				?>
-			</div>
-		<?php
-		$anniversary_paged = ( get_query_var( 'paged' ) ) ? absint( get_query_var( 'paged' ) ) : 1;
-		$total_pages = $total_pages;
-		$total_pages = ceil( $total_pages / 20 );
-		?>
-		</div>
-		<div class="cgl-ajax-number-pagination">
-			<?php
-				echo paginate_links( array(
-					// 'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-					'base' => 'https://api.themoviedb.org/3/movie/popular?api_key=94413492db5e2e4ca5e93402ca623fca&language=en-US'.'%_%',
-					'format' => '&page=%#%',
-					'number' => 3,
-					// 'current' => max( 1, get_query_var('paged') ),
-					'current' => $anniversary_paged,
-					'total' =>  $total_pages,
-				) );
-			?>
-		</div>
-		<?php
-		endif;
-		
+		$this->movie_list_callback_instance->handle_movie_list_block_content_from_api($block_attributes, $content);
 		$output = ob_get_clean();
 		return $output;
 	}
-	public function handle_movie_ajax_pagination(){
-		$get_movie_data = wp_remote_get( 'https://api.themoviedb.org/3/movie/popular?api_key=94413492db5e2e4ca5e93402ca623fca&language=en-US&page='. $_POST['pageNumber'] );
-		$pagination_body = '';
-		$total_pages = '';
-		if ( is_array( $get_movie_data ) && ! is_wp_error( $get_movie_data ) ) {
-			// $headers = $get_movie_data['headers']; // array of http header lines
-			$pagination_body    = json_decode($get_movie_data['body']); // use the content
-			$total_pages = $pagination_body->total_pages;
-		}
-		if($pagination_body):
-		?>
-			<div class="movie-list">
-				<?php 
-					foreach($pagination_body->results as $movie):
-				?>
-					<div class="movie-card">
-						<div class="movie-image">
-							<img src="https://image.tmdb.org/t/p/w500/<?php echo $movie->poster_path; ?>" alt="<?php echo $movie->title; ?>">
-						</div>
-						<div class="movie-content">
-							<h2><?php echo $movie->title; ?></h2>
-							<p class="overview"><?php echo $movie->overview; ?></p>
-						</div>
-					</div>
-				<?php
-					endforeach;
-				?>
-			</div>
-		<?php
-		$anniversary_paged = $_POST['pageNumber'];
-		$total_pages = $total_pages;
-		$total_pages = ceil( $total_pages / 20 );
-		?>
-		</div>
-		<div class="cgl-ajax-number-pagination">
-			<?php
-				echo paginate_links( array(
-					// 'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-					'base' => 'https://api.themoviedb.org/3/movie/popular?api_key=94413492db5e2e4ca5e93402ca623fca&language=en-US'.'%_%',
-					'format' => '&page=%#%',
-					'number' => 3,
-					// 'current' => max( 1, get_query_var('paged') ),
-					'current' => $anniversary_paged,
-					'total' =>  $total_pages,
-				) );
-			?>
-		</div>
-		<?php
-		endif;
-		die();
-	}
+	
 }
 
 ?>
