726bf5d0b06195099879a447addded3e8191e706
anam <anam.hossain@multidots.com>
2024-07-15 18:32:08 +0600

    [update] remove commented code

--- src/blocks/movie-lists/view.js
+++ src/blocks/movie-lists/view.js
@@ -1,21 +1,9 @@
-import React from 'react';
-
-// const MovieCard = ({ movie }) => {
-// 	console.log(movie);
-// };
-// console.log('movie', 'movie');
-
-// export default MovieCard;
-
 (function ($) {
 	$(document).ready(function () {
-		// <MovieCard movie={'movie'} />;
-		// console.log(<MovieCard movie={'movie'} />);
 		function getPageNumberFromUrl(url) {
 			// Extract query parameters from the URL
 			// var queryParams = getPageNumberFromUrl(anchor.href)
 			var queryParams = url.split('&');
-			// debugger;
 			// Iterate through query parameters to find 'page' parameter
 			for (var i = 0; i < queryParams.length; i++) {
 				var param = queryParams[i].split('=');
@@ -31,12 +19,9 @@ import React from 'react';
 			'.movie-list-ajax-number-pagination a',
 			function (e) {
 				e.preventDefault();
-				console.log('firing ajax');
-				// debugger;
 				var page = $(this).attr('href');
 				const urlParams = new URL(page).searchParams;
 				const blockId = urlParams.get('blockId');
-				// debugger;
 
 				var pageNumber = getPageNumberFromUrl(page);
 				pageNumber = pageNumber ? pageNumber : 1;
