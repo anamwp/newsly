f124f2fc277cf87b5dfe965845c09e33ddc015fd
anam <anam.hossain@multidots.com>
2025-02-17 15:49:18 +0600

    [update] add test case

---
+++ tests/test-class-movie-list-callback.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Anam\GutenbergStarter\Blocks\Inc\Class_Movie_List_Callback;
+
+class Test_Class_Movie_List_Callback extends TestCase {
+
+	public function test_handle_genre_filter() {
+		// Create an instance of the class
+		$movie_list_callback = new Class_Movie_List_Callback();
+
+		// Set up the genres
+		$movie_list_callback->new_genres = [
+			1 => ['id' => 1, 'name' => 'Action'],
+			2 => ['id' => 2, 'name' => 'Comedy'],
+			3 => ['id' => 3, 'name' => 'Drama'],
+		];
+
+		// Define the input array.
+		$input = [1, 3];
+
+		// Call the function.
+		$result = $movie_list_callback->handle_genre_filter($input);
+
+		// Define the expected result.
+		$expected = [
+			1 => ['id' => 1, 'name' => 'Action'],
+			3 => ['id' => 3, 'name' => 'Drama'],
+		];
+
+		// Assert that the result matches the expected output
+		$this->assertEquals($expected, $result);
+	}
+}
