3251638abf46e781fc15e690027b46f0d28babf7
anam <anam.hossain@multidots.com>
2025-03-11 00:12:25 +0600

    [delete] test folder remove from block dir

--- build/blocks/_test/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'eca3a2cdbf434b92ba64');
