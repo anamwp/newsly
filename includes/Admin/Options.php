4abcc041303645f9225e3e185068e5a5ab67e334
anamwp <theanamhossain@gmail.com>
2025-08-06 11:10:57 +0600

    [update] update api options from api-config to options php file

--- includes/Admin/Options.php
+++ includes/Admin/Options.php
@@ -15,27 +15,99 @@ class Options {
 	 * @var [type]
 	 */
 	private static $instance;
+	
 	public static function init() {
 		if ( null === self::$instance ) {
 			self::$instance = new self();
 		}
 		return self::$instance;
 	}
+	
 	/**
 	 * Construct of Class
 	 */
 	public function __construct() {
 		if ( is_admin() ) {
 			add_action( 'admin_menu', array( $this, 'gs_plugin_settings_page' ) );
+			add_action( 'admin_init', array( $this, 'register_settings' ) );
 		}
 	}
-	public function gs_plugin_settings_page() {
-		add_options_page( 'Gutenberg Starter', 'Gutenberg Starter', 'manage_options', 'guternberg_starter', array( $this, 'gs_handle_plugin_setting_page_callback' ), null );
+	
+	/**
+	 * Register plugin settings
+	 */
+	public function register_settings() {
+		// Movie Lists Block API Key
+		register_setting(
+			'gutenberg_starter_settings',
+			'gutenberg_starter_movie_api_key',
+			array(
+				'type'              => 'string',
+				'description'       => __( 'The Movie Database API Key', 'gutenberg-starter' ),
+				'sanitize_callback' => 'sanitize_text_field',
+				'show_in_rest'      => false, // Don't expose in REST API for security.
+				'default'           => '',
+			)
+		);
+		
+		// Add more settings here as needed for other blocks
 	}
+	
+	/**
+	 * Add settings page to admin menu
+	 */
+	public function gs_plugin_settings_page() {
+		add_options_page( 
+			__( 'Gutenberg Starter Settings', 'gutenberg-starter' ),
+			__( 'Gutenberg Starter', 'gutenberg-starter' ), 
+			'manage_options', 
+			'gutenberg_starter', 
+			array( $this, 'gs_handle_plugin_setting_page_callback' ), 
+			null 
+		);
+	}
+	
+	/**
+	 * Settings page callback
+	 */
 	public function gs_handle_plugin_setting_page_callback() {
+		// Get the API key.
+		$api_key = get_option( 'gutenberg_starter_movie_api_key', '' );
 		?>
-		<h1>Guternberg Starter</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam quam dicta consequatur labore, libero veniam suscipit obcaecati eligendi architecto veritatis, illum tempore harum quos assumenda ab, ut in consectetur aspernatur dolor at vel! Magni, accusamus?</p>
+		<div class="wrap">
+			<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+			
+			<form method="post" action="options.php">
+				<?php settings_fields( 'gutenberg_starter_settings' ); ?>
+				<?php do_settings_sections( 'gutenberg_starter_settings' ); ?>
+				
+				<h2><?php esc_html_e( 'Movie Lists Block Settings', 'gutenberg-starter' ); ?></h2>
+				<table class="form-table">
+					<tr>
+						<th scope="row">
+							<label for="gutenberg_starter_movie_api_key">
+								<?php esc_html_e( 'The Movie Database API Key', 'gutenberg-starter' ); ?>
+							</label>
+						</th>
+						<td>
+							<input type="text" 
+								id="gutenberg_starter_movie_api_key" 
+								name="gutenberg_starter_movie_api_key" 
+								value="<?php echo esc_attr( $api_key ); ?>" 
+								class="regular-text" 
+							/>
+							<p class="description">
+								<?php esc_html_e( 'Enter your API key from The Movie Database (TMDb). Get one at https://www.themoviedb.org/settings/api.', 'gutenberg-starter' ); ?>
+							</p>
+						</td>
+					</tr>
+				</table>
+				
+				<!-- Add more settings sections here for other blocks -->
+				
+				<?php submit_button(); ?>
+			</form>
+		</div>
 		<?php
 	}
 }
--- src/blocks/movie-lists/api-config.php
+++ src/blocks/movie-lists/api-config.php
@@ -10,78 +10,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Register and localize API settings.
- */
-function gutenberg_starter_register_movie_api_settings() {
-	// Register settings.
-	register_setting(
-		'gutenberg_starter_movie_settings',
-		'gutenberg_starter_movie_api_key',
-		array(
-			'type'              => 'string',
-			'description'       => __( 'The Movie Database API Key', 'gutenberg-starter' ),
-			'sanitize_callback' => 'sanitize_text_field',
-			'show_in_rest'      => false, // Don't expose in REST API for security.
-			'default'           => '',
-		)
-	);
-
-	// Add settings page.
-	add_action( 'admin_menu', 'gutenberg_starter_add_movie_settings_page' );
-}
-add_action( 'init', 'gutenberg_starter_register_movie_api_settings' );
-
-/**
- * Add settings page under Settings menu.
- */
-function gutenberg_starter_add_movie_settings_page() {
-	add_options_page(
-		__( 'Movie Lists Block Settings', 'gutenberg-starter' ),
-		__( 'Movie Lists Block', 'gutenberg-starter' ),
-		'manage_options',
-		'gutenberg-starter-movie-settings',
-		'gutenberg_starter_movie_settings_page'
-	);
-}
-
-/**
- * Settings page callback.
- */
-function gutenberg_starter_movie_settings_page() {
-	// Get the API key.
-	$api_key = get_option( 'gutenberg_starter_movie_api_key', '' );
-	?>
-	<div class="wrap">
-		<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
-		<form method="post" action="options.php">
-			<?php settings_fields( 'gutenberg_starter_movie_settings' ); ?>
-			<table class="form-table">
-				<tr>
-					<th scope="row">
-						<label for="gutenberg_starter_movie_api_key">
-							<?php esc_html_e( 'The Movie Database API Key', 'gutenberg-starter' ); ?>
-						</label>
-					</th>
-					<td>
-						<input type="text" 
-							id="gutenberg_starter_movie_api_key" 
-							name="gutenberg_starter_movie_api_key" 
-							value="<?php echo esc_attr( $api_key ); ?>" 
-							class="regular-text" 
-						/>
-						<p class="description">
-							<?php esc_html_e( 'Enter your API key from The Movie Database (TMDb). Get one at https://www.themoviedb.org/settings/api.', 'gutenberg-starter' ); ?>
-						</p>
-					</td>
-				</tr>
-			</table>
-			<?php submit_button(); ?>
-		</form>
-	</div>
-	<?php
-}
-
-/**
  * Enqueue block assets with localized API settings.
  */
 function gutenberg_starter_enqueue_movie_assets() {
@@ -95,10 +23,10 @@ function gutenberg_starter_enqueue_movie_assets() {
 			'gutenberg-starter-movie-lists', // Block script handle.
 			'movieListsBlockConfig',
 			array(
-				'apiKey'      => $api_key,
-				'apiUrl'      => 'https://api.themoviedb.org/3',
+				'apiKey'       => $api_key,
+				'apiUrl'       => 'https://api.themoviedb.org/3',
 				'imageBaseUrl' => 'https://image.tmdb.org/t/p/w500',
-				'nonce'       => wp_create_nonce( 'gutenberg-starter-movie-lists' ),
+				'nonce'        => wp_create_nonce( 'gutenberg-starter-movie-lists' ),
 			)
 		);
 	}
